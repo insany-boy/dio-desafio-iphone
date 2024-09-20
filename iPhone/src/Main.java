2c6e51d096811fd6089c9abd7d269344770c4e91
insany-boy <kauanmeirelles1008@gmail.com>
2024-09-20 11:43:50 -0300

    concluindo desafio

--- iPhone/src/Main.java
+++ iPhone/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        iPhone meuiPhone = new iPhone();
+
+        //Testando reprodutor musical
+        meuiPhone.selecionarMusica("Natural - Imagine Dragons");
+        meuiPhone.tocar();
+        meuiPhone.pausar();
+
+       //Testando Aparelho Telefonico
+
+       meuiPhone.ligar("71988471991");
+       meuiPhone.atender();
+       meuiPhone.iniciarCorreioVoz();
+
+       //Testando NavegadorInternet
+       meuiPhone.exibirPagina("www.youtube.com.br");
+       meuiPhone.adicionarNovaAba();
+       meuiPhone.atualizarPagina();
     }
 }
